2d8b388d118eac5e904f2bcfd904c38b055bf76b
JohndevBr <jca.oliveira91@gmail.com>
2020-03-12 20:22:04 +0000

    Desafio 1 módulo 2

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function criarQuadrado(){
+  quadradoVermelho = document.createElement('div')
+  quadradoVermelho.classList.add('quadrado')
+  
+  document.body.appendChild(quadradoVermelho);
+}
+
